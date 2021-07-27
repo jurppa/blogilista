261818704ba57ae47a44576c275d89e29dabbb31
jurppa <78047234+jurppa@users.noreply.github.com>
2021-07-27 14:12:23 +0300

    4.3 apufunktioita ja yksikkötestejä

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const mongoUrl = process.env.MONGODB_URI
+console.log(mongoUrl)
+const blogRouter = require('./controllers/blogs')
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => console.log('connected to mongodb')).catch((error) => console.log('error in connection:', error.message)) 
+app.use(cors())
+app.use(express.json())
+
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const app = require('./app') // varsinainen Express-sovellus
+const config = require('./utils/config')
 
-const mongoUrl = process.env.MONGODB_URI
-console.log(mongoUrl)
-const blogRouter = require('./controllers/blogs')
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => console.log('connected to mongodb')).catch((error) => console.log('error in connection:', error.message)) 
-app.use(cors())
-app.use(express.json())
-
-
-app.use('/api/blogs', blogRouter)
-
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
+    
 })
