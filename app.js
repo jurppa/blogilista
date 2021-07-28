fb8f007dfaf2695a771bf5b33db8706eaecef764
jurppa <78047234+jurppa@users.noreply.github.com>
2021-07-28 17:49:28 +0300

    4.15 blogilistan laajennus

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const usersRouter = require('./controllers/users')
 const mongoUrl = process.env.MONGODB_URI
 const blogRouter = require('./controllers/blogs')
 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 
-app.use('/api/blogs', blogRouter)
 
+app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 module.exports = app
---
+++ controllers/users.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (req, res) => {
+    const body = req.body
+    if(body.username.length < 3 || body.password.length < 3) 
+    {
+        res.status(400).send('username and password must be over 3 characters')
+    }
+    else {
+        const saltRounds = 10
+        const passwordHash = await bcrypt.hash(body.password, saltRounds)
+        const user = new User({
+            username: body.username,
+            name: body.name,
+            passwordHash,
+        })
+
+    
+        try {
+            const savedUser = await user.save()
+
+            res.json(savedUser)
+        } catch (error) {
+            res.status(400).send('Username must be unique.')
+       
+        }
+    }
+})
+usersRouter.get('/', async(req, res) => {
+    const users = await  User
+        .find({})
+    res.json(users.map(blog => blog.toJSON()))
+})
+
+module.exports = usersRouter
